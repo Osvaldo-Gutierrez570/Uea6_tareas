b40210a60cb16d25a7584e879816c2fb0cf80cf5
Osvaldo-Gutiérrez570 <2213025688@correo.ler.uam.mx>
2025-06-04 18:53:40 -0600

    correccion de codigo

--- interactividad1/sketch.js
+++ interactividad1/sketch.js
@@ -1,69 +1,76 @@
-let frase1 = "Presiona X";
-let frase2 = "";
-let frase3 = "";
 let color1;
-let color2;
-let color3;
-let colorb =0;
+let frase1 = "Presiona X";
+let texto1; 
+let posX, posY;
+let colorb;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  color1 = color(255); 
-  color2 = color(255); 
-  color3 = color(255);
-//  fondo1 = background(0);
+  color1 = color(255);
+  texto1 = 60;  // tamaño inicial
+  posX = width * 0.5;
+  posY = height * 0.5;
+ colorb = color(0);
+
 }
 
+ if (frase1 === "Presiona X") {
+    stroke(255); // color blanco
+    strokeWeight(2); // grosor de línea
+    line(0, 0, width, height);
+    line(width, 0, 0, height);
+    line(width / 2, 0, width / 2, height);
+    line(0, height / 2, width, height / 2);
+  }
+
 function draw() {
-background(colorb);
- 
+  background(colorb);
 
-
-  // Frase 1
+  textAlign(CENTER, CENTER);
   fill(color1);
-  textSize(60);
-  text(frase1, 800, 300);
-
-  // Frase 2
-  fill(color2);
-  textSize(80);
-  text(frase2, 500, 500);
-
-    // Frase 3
-  fill(color3);
-  textSize(100);
-  text(frase3, 900, 900); 
+  textSize(texto1);
+  text(frase1, posX, posY);
 }
 
 function keyPressed() {
   if (key === 'x') {
     frase1 = "Presiona P";
-    color1 = color(255, 0, 0); 
-    colorb = 255;
+    colorb = color(237, 242, 244); // color fondo
+    color1 = color(0, 0, 255); // color de texto
+    texto1 = random(30, 100); // tamaño de texto 
+    posX = 500; //posicion 
+    posY = 100; 
+
+
+  } else if (key === 'p') {
+    frase1 = "Presiona W";
+    color1 = color(239, 35, 60);
+    texto1 = random(30, 100);
+    posX = 800; //posicion 
+    posY = 500; 
+
+
+  } else if (key === 'w') {
+    frase1 = "Presiona A";
+    color1 = color(0, 255, 0);
+    texto1 = random(30, 100);
+    posX = 400; //posicion 
+    posY = 400; 
+    
+
+
+  } else if (key === 'a') {
+    frase1 = "Presiona S";
+    color1 = color(141, 153, 174);
+    texto1 = random(30, 100);
+
+
+  } else if (key === 's') {
+    frase1 = "Presiona x";
+    colorb = color(0);
+    color1 = color(255);
+    texto1 = (60);
+    posX = width * 0.5;
+    posY = height * 0.5;
   }
-
-  if (key === 'p') {
-    frase2 = "Presiona W";
-
-    frase1 = "Presiona P";
-    color1 = color (0);
-
-    color2 = color (0,255,0)
-    colorb = 0;
-  }
-
-    if (key === 'w') {
-    frase1 = "Presiona P";
-    color1 = color (255);
-
-    color1 = color(255, 0, 0); 
-    colorb = 255;
-
-  }
-    if (key === 'w') {
-    frase1 = "Presiona 3";
-      frase3 = "Presiona S";
-    color3 = color (0,0,255)
-    colorb = (0,0,255);
-}
 }
